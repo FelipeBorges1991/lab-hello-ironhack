f08978b0875704cc381dc1f816c6821fc16c50dc
Felipe Borges <fcborges_@hotmail.com>
2021-04-13 20:43:27 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import IronhackLogo from "./ironhack-logo.svg";
+import MenuTop from "./menu-top.svg";
+import reactLogo from "./react-logo.svg";
+import icon1 from "./icon1.png";
+import icon2 from "./icon2.png";
+import icon3 from "./icon3.png";
+import icon4 from "./icon4.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="firstPage">
+        <div className="NavBar">
+          <img src={IronhackLogo} />
+          <img src={MenuTop} />
+        </div>
+        <img className="imgReact" src={reactLogo} />
+        <div>
+          <h1>Say hello to ReactJS</h1>
+          <p>
+            You will learn how to use the most popular frontend library, and
+            become a super Ninja developer.
+          </p>
+          <button>Awesome!</button>
+        </div>
+      </section>
+
+      <section className="secondPage">
+        <div>
+          <img src={icon1} />
+          <h2>Declarative</h2>
+          <p>React makes it painless to create interactive UIs.</p>
+        </div>
+
+        <div>
+          <img src={icon2} />
+          <h2>Components</h2>
+          <p>Build encapsulated compnents that manage their state.</p>
+        </div>
+
+        <div>
+          <img src={icon3} />
+          <h2>Single-Way</h2>
+          <p>A set of immutable values are passed to the component's</p>
+        </div>
+
+        <div>
+          <img src={icon4} />
+          <h2>JSX</h2>
+          <p>Statically-typed, designed to run on modern browsers.</p>
+        </div>
+      </section>
     </div>
   );
 }
